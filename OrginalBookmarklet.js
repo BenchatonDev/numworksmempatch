70e03396517742a95845710efe6b44bc32344c38
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2023-10-04 17:56:14 +0200

    Added OrginalBookmarklet.js

---
+++ OrginalBookmarklet.js
@@ -0,0 +1,9 @@
+(function() {
+    !function() {
+        let script = document.createElement("script");
+        script.type = "text/javascript",
+        script.src = "https://raw.githubusercontent.com/benchatondev/numworksmempatch/main/nwagra.min.js",
+        document.head.appendChild(script)
+    }();
+}
+)();
