e9c698d1f6a5344c4021d596b59f40757b629af0
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2023-10-05 18:13:06 +0200

    Removed stupid thing I had added NewBookmarklet.js

--- NewBookmarklet.js
+++ NewBookmarklet.js
@@ -23,7 +23,6 @@ function Patch() {
   let opener = _____WB$wombat$assign$function_____("opener");
 
   (function(){let e="https://my.numworks.com/apps";if(document.getElementById("nwagra")){alert("You should avoid prolonged exposure to Nwagra!");return}let t=document.querySelector("a.btn.btn-primary");if(t){let n=USBDevice.prototype.controlTransferIn;USBDevice.prototype.controlTransferIn=function(e,t){if(t!=40)return n.call(this,e,t);let s=this;return new Promise(function(o){n.call(s,e,t).then(function(e){e.data.getUint32()==4276994270&&e.data.setUint32(24,2424307712,!0),o(e)})})},document.querySelector("h1").innerHTML="Enlarged Extra Apps";let e=document.createElement("p");e.id="nwagra",e.className="text-center",e.style.marginTop="50px";let t=document.createElement("img");t.src="https://web.archive.org/web/20221008080116/https://www.nwagyu.com/nwagra.jpg",t.width="150",e.appendChild(t),document.querySelector("section").appendChild(e)}else alert("Please unplug your calculator and start over."),window.location.href=e})()
-  alert("Enlarged apps Successfully enabled")
   }  
 }
 
